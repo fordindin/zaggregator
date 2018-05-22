19048b1b15a0afa8e1b0df87d783bef8dcaa1bb4
Denis Barov <dindin@dindin.ru>
2018-05-22 10:48:09 -0400

    fix exit code in make_env.sh script

--- make_env.sh
+++ make_env.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-which virtualenv > /dev/null 2>&1 || echo "Can't find virtualenv utility in PATH" && exit(-1)
+which virtualenv > /dev/null 2>&1 || ( echo "Can't find virtualenv utility in PATH" ; exit 255 )
 
 virtualenv -p python3 venv
 . ./venv/bin/activate
