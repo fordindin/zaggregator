8ca9e1d1bc781ed682cad6b86394220494e73db2
Denis Barov <dindin@dindin.ru>
2018-05-23 12:43:06 -0400

    fix empty variables in venv/bin/activate

--- make_env.sh
+++ make_env.sh
@@ -5,5 +5,7 @@ set -eu
 which virtualenv > /dev/null 2>&1 || ( echo "Can't find virtualenv utility in PATH" ; exit 255 )
 
 virtualenv -p python3 venv
+set +eu
 . ./venv/bin/activate
+set -eu
 pip install -r requirements.txt
