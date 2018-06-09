4b3471e26b0c049c5933df10c9f571ac741dd12e
Denis Barov <dindin@dindin.ru>
2018-06-09 12:47:08 -0400

    daemon start

--- zaggregator/__main__.py
+++ zaggregator/__main__.py
@@ -1,48 +1,6 @@
 #!/usr/bin/env python
 
-import psutil
-import sys
-import logging
-from fuzzywuzzy import fuzz
-from zaggregator.procbundle import ProcTable, EmptyBundle
-from zaggregator.utils import discovery_json
-import json
-
+from zaggregator.daemon import start
 
 if __name__ == '__main__':
-
-    if len(sys.argv) > 0:
-        try:
-            pt = ProcTable()
-        except EmptyBundle:
-            print("0.0")
-            sys.exit(0)
-        if sys.argv[1] == 'discovery':
-            print(discovery_json(pt.get_bundle_names()))
-        elif sys.argv[1] == 'idle' and sys.argv[2] == 'pcpu':
-            print(pt.get_idle())
-        else:
-            if sys.argv[1] in pt.get_bundle_names():
-                bundle = pt.get_bundle_by_name(sys.argv[1])
-                if not bundle:
-                    print("0.0")
-                    sys.exit(0)
-                metrics = sys.argv[2]
-                #if metrics == "nconn":
-                #    print(bundle.get_n_connections())
-                #if metrics == "nfd":
-                #    print(bundle.get_n_fds())
-                #if metrics == "nfile":
-                #    print(bundle.get_n_open_files())
-                if metrics == "ctxswvol":
-                    print(bundle.get_n_ctx_switches_vol())
-                if metrics == "ctxswinvol":
-                    print(bundle.get_n_ctx_switches_invol())
-                if metrics == "memrss":
-                    print(bundle.get_memory_info_rss())
-                if metrics == "pcpu":
-                    bundle.set_cpu_percent()
-                    print(bundle.get_cpu_percent())
-            else:
-                print("0.0")
-                sys.exit(0)
+    start()
