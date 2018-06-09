dee2b9281a7675adaf869ba3881f88a57ae082c0
Denis Barov <dindin@dindin.ru>
2018-06-09 13:04:55 -0400

    better testing script

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+ASYNCIO_WAIT=3
+DAEMON_WAIT=5
+
 dir=$(dirname $0)/
 cd $dir
 set +eu
@@ -19,4 +22,42 @@ else
 fi
 set -u
 
-$SUDO python3 -m zaggregator.tests
+unittests(){
+    $SUDO python3 -m zaggregator.tests
+}
+
+
+echo "Running daemon-mode tests"
+
+killtest()
+{
+    set -eu
+
+    eval _wait="\$${1}"
+    sleep ${_wait}
+    kill -15 $testpid
+    set +eu
+}
+
+asyncio_test(){
+    echo "$(($ASYNCIO_WAIT-1)) cycles with dummmy asyncio server"
+    python3 ./zaggregator/tests/run_asyncio.py & testpid=$! && echo "PID: $testpid" && killtest ASYNCIO_WAIT &
+    wait
+    echo ""
+}
+
+daemon_test(){
+    echo "$(($DAEMON_WAIT-1)) cycles with real zaggregator server"
+    python3 -m zaggregator & testpid=$! && echo "PID: $testpid" && killtest DAEMON_WAIT &
+    wait
+    echo ""
+}
+
+_fkill(){
+    kill -15 $testpid
+}
+
+trap _fkill 1 2 4 15
+unittests
+#asyncio_test
+#daemon_test
