b09ba757c4896bab6d4e1a23bbaa1da9c6fe7ab6
Denis Barov <dindin@dindin.ru>
2018-06-15 02:39:51 -0400

    move requirements inside setup script

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
-    requirements = list(filter(lambda x: x!= "", fh.read().split('\n')))
-
 setuptools.setup(
     name="zaggregator",
     version="0.0.2",
@@ -22,7 +19,7 @@ setuptools.setup(
     ),
     keywords="zabbix monitoring",
     python_requires='>=3',
-    install_requires=requirements,
+    install_requires=['fuzzywuzzy>=0.16.0', 'psutil>=5.4.5', 'python-Levenshtein>=0.12.0', 'setproctitle>=1.1.10'],
     packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests*', 'venv', 'misc', 'build', 'dist']),
     zip_safe=True,
     entry_points={
