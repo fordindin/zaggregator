787941f78baa8ecba98c87e0927929b4c99e7d67
Denis Barov <dindin@dindin.ru>
2018-08-01 07:35:57 -0400

    improve release script

--- release.sh
+++ release.sh
@@ -23,7 +23,7 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-git status | grep 'working tree clean' || echo "Clean working tree first (see 'git status' output)" && exit 1
+git status | grep 'working tree clean' || ( echo "Clean working tree first (see 'git status' output)" && exit 1 )
 git tag zaggregator-$1-release
 git push --tags
 python3 setup.py egg_info -Db "" sdist bdist_egg upload
