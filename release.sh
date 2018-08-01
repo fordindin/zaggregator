c953c7bc4486a498013738069036e4fcfc32bfcd
Denis Barov <dindin@dindin.ru>
2018-08-01 11:19:32 -0400

    improve release script

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ usage(){
 
     example:
 
-        ./${selfname} 1.2.3
+        ./${selfname}
 
     will create tag zaggregator-1.2.3-release, push this tag to origin;
     than build pip package for zaggregator and push it to pipy repo
@@ -18,12 +18,16 @@ usage(){
 "
 }
 
-if [ $# -lt 1 ]; then
+set +u
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
     usage
     exit 1
 fi
+set -u
+
+version=$(grep 'version=' setup.py | cut -d= -f 2 | tr -d '",')
 
 git status | grep 'working tree clean' || ( echo "Clean working tree first (see 'git status' output)" && exit 1 )
-git tag zaggregator-$1-release
+git tag zaggregator-version-release
 git push --tags
 python3 setup.py egg_info -Db "" sdist bdist_egg upload
